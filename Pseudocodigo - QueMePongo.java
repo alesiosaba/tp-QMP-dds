f3ae180ac40594354b9f8cafaea1669f9f03814d
alesiosaba <alesiosaba@gmail.com>
2021-04-28 20:39:38 -0300

    agrego borrador para crear prendas

--- Pseudocodigo - QueMePongo.java
+++ Pseudocodigo - QueMePongo.java
@@ -3,17 +3,18 @@ class Prenda {
     Material material
     Color colorPrincipal
     Color colorSecundario
+    Trama trama
 
-    constructor(tipoDePrenda, material, colorPrincipal)
+    constructor(tipoDePrenda, material, colorPrincipal,colorSecundario,trama)
         this.tipoDePrenda = requireNonNull(tipoDePrenda, "tipo de prenda es obligatorio")
         this.material = requireNonNull(material, "material es obligatorio")
         this.colorPrincipal = requireNonNull(colorPrincipal, "color principal es obligatorio")
-
-    constructor(tipoDePrenda, material, colorPrincipal,colorSecundario)
-        this.tipoDePrenda = requireNonNull(tipoDePrenda, "tipo de prenda es obligatorio")
-        this.material = requireNonNull(material, "material es obligatorio")
-        this.colorPrincipal = requireNonNull(colorPrincipal, "color principal es obligatorio")
-        this.colorSecundario = requireNonNull(colorSecundario, "color secundario es obligatorio")
+        this.colorSecundario = colorSecundario
+        
+        if(trama == null)
+            this.trama = TipoDePrenda.LISA
+        else
+            this.trama = trama
 
     Categoria categoria()
         return this.tipoPrenda.categoria()
@@ -21,12 +22,20 @@ class Prenda {
 --------------------------------------------------------------------------------------------------------------
 class TipoDePrenda {
     Categoria categoria
+    TipoDePrenda tipoDePrenda
 
     constructor(Categoria categoria)
       this.categoria = categoria
 
     Categoria categoria()
         return this.categoria
+}
+--------------------------------------------------------------------------------------------------------------
+enum Categoria {
+    PARTE_SUPERIOR,
+    CALZADO,
+    PARTE_INFERIOR,
+    ACCESORIOS
 }
 --------------------------------------------------------------------------------------------------------------
 class Color {
@@ -40,10 +49,45 @@ class Color {
         this.azul = azul
 }
 --------------------------------------------------------------------------------------------------------------
-enum Categoria {
-    PARTE_SUPERIOR,
-    CALZADO,
-    PARTE_INFERIOR,
-    ACCESORIOS
+enum Material {
+    CUERO,
+    JEAN,
+    ALGODON,
+    ...
+}
+--------------------------------------------------------------------------------------------------------------
+enum Trama {
+    LISA,
+    RAYADA,
+    CON_LUNARES,
+    A_CUADROS,
+    ESTAMPADO
+}
+--------------------------------------------------------------------------------------------------------------
+enum TipoDePrenda {
+    ZAPATO,
+    REMERA,
+    CAMISA,
+    ...
+}
+--------------------------------------------------------------------------------------------------------------
+class Borrador {
+    TipoPrenda tipoPrenda
+    Material material
+    Color colorPrincipal
+    Color colorSecundario
+    Trama trama
+
+    void cargarTipoDePrenda(tipo)
+        this.TipoPrenda = tipo
+    
+    void cargarDatosRestantes(colorP,colorS,material,trama)
+        this.colorP = colorPrincipal
+        this.colorS = colorSecundario
+        this.material = material
+        this.trama = trama
+
+    Prenda crearPrenda()
+        return new Prenda(tipoPrenda,material,colorPrincipal,colorSecundario,trama)
 }
 --------------------------------------------------------------------------------------------------------------
